6635b4588483d16a94d5c86b12216711a715b2af
nithin-maker <76596018+nithin-maker@users.noreply.github.com>
2021-09-10 11:35:58 +0530

    Add files via upload

---
+++ Session 4.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the no:"))
+for n in range(2, n):
+     for x in range(2, n):
+         if n % x == 0:
+             print(n, 'equals', x, '*', n//x)
+             break
+     else:
+         # loop fell through without finding a factor
+         print(n, 'is a prime number')
